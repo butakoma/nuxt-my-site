1c6023af16c71a3e3e0a8c3bb0ef2295d74b9a86
butakoma <ky.tanabota@gmail.com>
2019-11-14 23:56:18 +0900

    add Header.vue

---
+++ components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <nuxt-link to="/">Home</nuxt-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
