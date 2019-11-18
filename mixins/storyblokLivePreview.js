f968a95a29198946a0477834d5050f64de03f24e
butakoma <ky.tanabota@gmail.com>
2019-11-18 23:59:49 +0900

    set index.vue component tag

--- mixins/storyblokLivePreview.js
+++ mixins/storyblokLivePreview.js
@@ -1,13 +1,12 @@
 export default {
-    mounted() {
-        this.$storybridge.on(['published', 'change'], (event) => {
-            if (!event.slugChanged) {
-                // Reload the page on save events (publish, save button or autosave)
-                this.$nuxt.$router.go({
-                    path: this.$nuxt.$router.currentRoute,
-                    force: true
-                })
-            }
+  mounted() {
+    this.$storybridge.on(['published', 'change'], (event) => {
+      if (!event.slugChanged) {
+        this.$nuxt.$router.go({
+          path: this.$nuxt.$router.currentRoute,
+          force: true
         })
-    }
+      }
+    })
+  }
 }
